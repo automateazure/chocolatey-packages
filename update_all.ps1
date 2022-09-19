fb920016d085d4fa6ea91ca0a04e91217045e52a
Brian Brito <brian.brito@ensemblehp.com>
2022-09-19 12:55:52 -0400

    edits

--- update_all.ps1
+++ update_all.ps1
@@ -1,6 +1,6 @@
 # AU Packages Template: https://github.com/majkinetor/au-packages-template
 
-param([string[]] $Name, [string] $ForcedPackages, [string] $Root = $PSScriptRoot/automatic)
+param([string[]] $Name, [string] $ForcedPackages, [string] $Root = "$PSScriptRoot/automatic")
 
 if (Test-Path $PSScriptRoot/update_vars.ps1) { . $PSScriptRoot/update_vars.ps1 }
 
@@ -10,20 +10,15 @@ $Options = [ordered]@{
     Timeout       = 100                                     #Connection timeout in seconds
     UpdateTimeout = 1200                                    #Update timeout in seconds
     Threads       = 10                                      #Number of background jobs to use
-    Push          = $Env:au_Push -eq 'true'                 #Push to chocolatey
+    Push          = $Env:au_Push -eq 'true'                #Push to chocolatey
     PushAll       = $true                                   #Allow to push multiple packages at once
     PluginPath    = ''                                      #Path to user plugins
     IgnoreOn      = @(                                      #Error message parts to set the package ignore status
-      'Origin Time-out'
       'Could not create SSL/TLS secure channel'
       'Could not establish trust relationship'
       'The operation has timed out'
       'Internal Server Error'
       'Service Temporarily Unavailable'
-      'the package version already exists on the repository'
-      'already exists on a Simple OData Server'             # https://github.com/chocolatey/chocolatey.org/issues/613
-      'Conflict'
-      'OutOfMemoryException'
     )
     RepeatOn      = @(                                      #Error message parts on which to repeat package updater
       'Could not create SSL/TLS secure channel'             # https://github.com/chocolatey/chocolatey-coreteampackages/issues/718
@@ -35,21 +30,19 @@ $Options = [ordered]@{
       'Internal Server Error'
       'An exception occurred during a WebClient request'
       'remote session failed with an unexpected state'
-      'the package version already exists on the repository'
-      'already exists on a Simple OData Server'             # https://github.com/chocolatey/chocolatey.org/issues/613
-      'Conflict'
-      'Job returned no object'
     )
-    #RepeatSleep   = 10                                     #How much to sleep between repeats in seconds, by default 0
-    #RepeatCount   = 5                                      #How many times to repeat on errors, by default 1
+    #RepeatSleep   = 250                                    #How much to sleep between repeats in seconds, by default 0
+    #RepeatCount   = 2                                      #How many times to repeat on errors, by default 1
+
     NoCheckChocoVersion = $true                            #Turn on this switch for all packages
+
     Report = @{
         Type = 'markdown'                                   #Report type: markdown or text
         Path = "$PSScriptRoot\Update-AUPackages.md"         #Path where to save the report
         Params= @{                                          #Report parameters:
             Github_UserRepo = $Env:github_user_repo         #  Markdown: shows user info in upper right corner
             NoAppVeyor  = $false                            #  Markdown: do not show AppVeyor build shield
-            UserMessage = "[Ignored](#ignored) | [History](#update-history) | [Force Test](https://gist.github.com/$Env:gist_id_test) | [Releases](https://github.com/$Env:github_user_repo/tags) | **TESTING AU NEXT VERSION**"       #  Markdown, Text: Custom user message to show
+            UserMessage = "[Ignored](#ignored) | [History](#update-history) | [Force Test](https://gist.github.com/$Env:gist_id_test) | [Releases](https://github.com/$Env:github_user_repo/tags)"       #  Markdown, Text: Custom user message to show
             NoIcons     = $false                            #  Markdown: don't show icon
             IconSize    = 32                                #  Markdown: icon size
             Title       = ''                                #  Markdown, Text: TItle of the report, by default 'Update-AUPackages'
@@ -71,7 +64,6 @@ $Options = [ordered]@{
     Git = @{
         User     = ''                                       #Git username, leave empty if github api key is used
         Password = $Env:github_api_key                      #Password if username is not empty, otherwise api key
-        # Branch   = 'updates'
     }
 
     GitReleases  = @{
@@ -87,13 +79,14 @@ $Options = [ordered]@{
     Mail = if ($Env:mail_user) {
             @{
                 To         = $Env:mail_user
+                From       = $Env:mail_from
                 Server     = $Env:mail_server
                 UserName   = $Env:mail_user
                 Password   = $Env:mail_pass
                 Port       = $Env:mail_port
                 EnableSsl  = $Env:mail_enablessl -eq 'true'
                 Attachment = "$PSScriptRoot\update_info.xml"
-                UserMessage = ""
+                UserMessage = ''
                 SendAlways  = $false                        #Send notifications every time
              }
            } else {}
@@ -113,7 +106,8 @@ $Options = [ordered]@{
 }
 
 if ($ForcedPackages) { Write-Host "FORCED PACKAGES: $ForcedPackages" }
-$global:au_Root = $Root                                    #Path to the AU packages
+$global:au_Root         = $Root          #Path to the AU packages
+$global:au_GalleryUrl   = ''             #URL to package gallery, leave empty for Chocolatey Gallery
 $global:info = updateall -Name $Name -Options $Options
 
 #Uncomment to fail the build on AppVeyor on any package error
